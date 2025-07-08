c0342309fdbf874311dc8b4120542b54ae47a27e
yusuf <yusuftri023@gmail.com>
2025-07-08 20:45:45 +0800

    change wp config

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -17,7 +17,7 @@
  *
  * @package WordPress
  */
-
+define('WP_MEMORY_LIMIT', '256M');
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 if (isset($_ENV['DATABASE'])) {
